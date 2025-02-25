ca2a56a7eac08757d441de10503a51ba3833d0d9
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-02-25 20:30:51 +0100

    update baner

--- components/marketing/banner.tsx
+++ components/marketing/banner.tsx
@@ -1,10 +1,15 @@
 export const Banner = () =>{
     return (
-    <div className="py-3 text-center bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6ff)]"  >
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,_214,_255,_ .7),rgb(41,_216,_255,_.7),rgb(255,_253,_128,_.7),rgb(248,_154,_191,_.7),rgb(252,_214,_255,_.7))]"  >
         <div className="containe">
-         <a href="#" className=" underline underline-offset-4 font-medium">
-            Explore the demo
+            <p className="font-medium">
+            <span className="hidden sm:inline">
+                konfikuracja komputera-</span>
+            <a href="#" className=" underline underline-offset-4 ">
+            banner
             </a>
+            </p>
+        
         </div>
     </div>
     );
