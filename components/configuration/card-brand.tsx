77d60c21d9895f41d526d633e312834d00478062
Numbers analyst <mrgregorybrzeczyszczykiewicz@proton.me>
2025-02-25 19:59:26 +0100

    Code refactor

---
+++ components/configuration/card-brand.tsx
@@ -0,0 +1,66 @@
+import { cn } from "@/lib/utils";
+import { MagicCard } from "../magicui/magic-card";
+import Image from "next/image";
+
+interface BrandCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  thing?: string;
+  selected?: boolean;
+  onClick?: () => void;
+  logoSrc: string;
+  logoAlt: string;
+  logoSize: number;
+  gradientColor: string;
+  gradientFrom: string;
+  gradientTo: string;
+  ringColor: string;
+  darkInvert?: boolean;
+  darkHueRotate?: boolean;
+}
+
+export const BrandCard = ({
+  className,
+  selected = false,
+  thing,
+  onClick,
+  logoSrc,
+  logoAlt,
+  logoSize,
+  gradientColor,
+  gradientFrom,
+  gradientTo,
+  ringColor,
+  darkInvert = true,
+  darkHueRotate = false,
+  ...props
+}: BrandCardProps) => {
+  return (
+    <MagicCard
+      className={cn(
+        "cursor-pointer flex-col items-center justify-center text-2xl shadow-2xl",
+        selected && `ring-1 ${ringColor} ring-opacity-60`,
+      )}
+      gradientOpacity={0.1}
+      gradientSize={300}
+      gradientColor={gradientColor}
+      gradientFrom={gradientFrom}
+      gradientTo={gradientTo}
+      onClick={onClick}
+      {...props}
+    >
+      <div className="relative">
+        <div className="flex justify-center items-center">
+          <Image
+            className={cn("dark:invert", darkHueRotate && "dark:hue-rotate-180")}
+            src={logoSrc}
+            alt={logoAlt}
+            width={logoSize}
+            height={logoSize}
+          />
+        </div>
+        <div className="flex justify-center items-center text-nowrap text-gray-500 lg:absolute text-center w-full mt-2 lg:mt-5">
+          {thing}
+        </div>
+      </div>
+    </MagicCard>
+  );
+};
--- components/configuration/select-cpu-brand.tsx
+++ components/configuration/select-cpu-brand.tsx
@@ -2,8 +2,7 @@
 
 import { useStoreSelectors } from "@/stores/store";
 import { scrollToElement } from "@/lib/utils";
-import { AmdBrandCard } from "./card-amd-brand";
-import { IntelBrandCard } from "./card-intel-brand";
+import { BrandCard } from "./card-brand";
 
 export const SelectCpuBrand = () => {
   const selectedCpuBrand = useStoreSelectors.use.selectedCpuBrand();
@@ -11,19 +10,36 @@ export const SelectCpuBrand = () => {
 
   return (
     <div className="flex h-[500px] lg:h-[400px] flex-col gap-4 lg:flex-row select-none">
-      <AmdBrandCard
+      <BrandCard
+        thing="AMD processors"
         selected={selectedCpuBrand === "AMD"}
         onClick={() => {
           setCpuBrand("AMD");
           scrollToElement("#cpu-carousel");
         }}
+        logoSrc={"/amd-logo.svg"}
+        logoAlt="AMD brand logo"
+        logoSize={270}
+        gradientColor="#ff0000"
+        gradientFrom="#ffc0c0"
+        gradientTo="#ff9980"
+        ringColor="ring-red-500"
       />
-      <IntelBrandCard
+      <BrandCard
+        thing="Intel processors"
         selected={selectedCpuBrand === "Intel"}
         onClick={() => {
           setCpuBrand("Intel");
           scrollToElement("#cpu-carousel");
         }}
+        logoSrc={"/intel-logo.svg"}
+        logoAlt="Intel brand logo"
+        logoSize={200}
+        gradientColor="#0099ff"
+        gradientFrom="#ccccff"
+        gradientTo="#66ccff"
+        ringColor="ring-blue-500"
+        darkHueRotate={true}
       />
     </div>
   );
