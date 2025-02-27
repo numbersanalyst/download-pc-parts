739c41a926f0a53d56e7956f8ef53a08d8a48c63
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-02-27 17:08:52 +0100

    add logo

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
-import { Banner } from "@/components/marketing/banner";
+import { Banner } from "@/components/Banner";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>
-        <Banner/>
-    </div>
+    <>
+      <Banner />
+      <Navbar />
+    </>
   );
 }
