a9dfbf62fd78cf97ddbae8666960ff71692ba99b
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-03-12 19:06:28 +0100

    update baner

--- components/marketing/navbar.tsx
+++ components/marketing/navbar.tsx
@@ -4,7 +4,8 @@ import menuIcon from "@/public/icon/menu.svg";
 
 export const Navbar = () => {
   return (
-    <div className="container bg-black">
+    <div className="bg-black">
+    <div className="container ">
       <div className="py-4 flex items-center justify-between">
         <div className="relative">
           <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD98,#C2F0B1,#2FD8FE)]"></div>
@@ -12,8 +13,17 @@ export const Navbar = () => {
         </div>
         <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg">
           <Image src={menuIcon} alt="Menu icon" className="h-6 w-6" />
-        </div>
+        </div>|
+        <nav className="flex gap-6 items-center">
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">About</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Upadates</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Help</a>
+          <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Customers</a>
+          <button className="bg-white py-2 px-4 rounded-lg">Get for free</button>
+        </nav>
       </div>
+     </div>
     </div>
   );
 };
