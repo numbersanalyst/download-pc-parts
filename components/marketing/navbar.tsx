260346f0413022fb1e02600ce3ceaa2bba27b1da
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-03-12 19:06:42 +0100

    update baner

--- components/marketing/navbar.tsx
+++ components/marketing/navbar.tsx
@@ -5,7 +5,7 @@ import menuIcon from "@/public/icon/menu.svg";
 export const Navbar = () => {
   return (
     <div className="bg-black">
-    <div className="container ">
+    <div className="px-4 ">
       <div className="py-4 flex items-center justify-between">
         <div className="relative">
           <div className="absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD98,#C2F0B1,#2FD8FE)]"></div>
@@ -14,7 +14,7 @@ export const Navbar = () => {
         <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-lg">
           <Image src={menuIcon} alt="Menu icon" className="h-6 w-6" />
         </div>|
-        <nav className="flex gap-6 items-center">
+        <nav className="gap-6 items-center hidden sm:flex">
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">About</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Features</a>
           <a href="#" className="text-opacity-60 text-white hover:text-opacity-100 transition">Upadates</a>
