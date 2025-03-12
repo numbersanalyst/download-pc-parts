64606b3412ef51ab88c8d106d8e9a2654d187a0e
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-03-12 19:39:22 +0100

    center

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,13 @@
 import { Banner } from "@/components/marketing/banner";
 import { Navbar } from "@/components/marketing/navbar";
+import { Hero } from "@/components/marketing/hero";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ components/marketing/hero.tsx
@@ -0,0 +1,18 @@
+export const Hero = () => {
+    return (
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)]">
+    <div className="container">
+        <a href="#">
+            <span>Version 2.0 is here</span>
+            <span>Read More</span>
+        </a>
+        <h1>One Task at a Time</h1>
+        <p>
+            Celebrate the joy of accomplishment with an app designed to track 
+            progress, motivate your efforts, and celebrate your successes.
+        </p>
+        <button>Get for free</button>
+    </div>
+    </div>
+    );
+};
