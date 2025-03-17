59b21f72055578b222657dcb1110323d5a1b57e6
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-03-18 00:05:31 +0100

    update center

--- components/marketing/hero.tsx
+++ components/marketing/hero.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image"; // Używamy Next.js Image dla optymalizacji
 import logoImage from "@/public/logos/logo.png";
+import CPUImage from "@/public/logos/intel-core-i9-9900ks.png";
+import GPUImage from "@/public/logos/rtx-4090ti.png";
 
 export const Hero = () => {
     return (
-        <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] relative overflow-hidden">
-            <div className="container mx-auto max-w-5xl px-4">
+        
+    <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-hidden">
+            <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] rounded-[100%] left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+            <div className="container relatve">
                 <div className="flex items-center justify-center">
                     <a href="#" className="inline-flex gap-3 border py-1 px-3 rounded-lg border-white/30">
                         <span className="bg-[linear-gradient(to_right,#F87AFF,#FB93D0,#FFDD99,#C3F0B2,#2FD8FE)] text-transparent bg-clip-text">
@@ -16,20 +20,24 @@ export const Hero = () => {
                         </span>
                     </a>
                 </div>
-                <h1 className="text-7xl font-bold tracking-tighter text-center mt-8">
-                    One Task at a Time
+                <div className="flex justify-center">
+                <h1 className="text-7xl sm:text-9xl font-bold tracking-tighter text-center mt-8 inline-flex">
+                    One Task 
+                    <br/>at a Time
                 </h1>
-                <p className="text-center text-xl mt-8">
+                </div>
+                <div className="flex justify-center">
+                <p className="text-center text-xl mt-8 max-w-md">
                     Celebrate the joy of accomplishment with an app designed to track 
                     progress, motivate your efforts, and celebrate your successes.
                 </p>
+                </div>
                 <div className="flex justify-center mt-8">
                     <button className="bg-white text-black py-3 px-6 rounded-lg font-medium">
                         Get for free
                     </button>
                 </div>
             </div>
-            <div className="absolute h-[375px] w-[750px] rounded-[100%] left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)]"></div>
         </div>
     );
 };
