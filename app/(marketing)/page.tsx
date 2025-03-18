b54f9c5f26b143db24d270cbb7c25708acdc0825
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-03-18 01:18:07 +0100

    update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 import { RainbowBanner } from "@/components/marketing/banner";
 import { Navbar } from "@/components/marketing/navbar";
 import { Hero } from "@/components/marketing/hero";
-import { LogoTicker } from "@/components/marketing/logoticker";
+import { LogoTicker } from "@/components/marketing/logoticker.tsx";
 
 export default function Home() {
   return (
