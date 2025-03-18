184449e273516bde0756a8856cc9878d0ecbdce8
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-03-18 01:30:49 +0100

    update

--- components/marketing/features.tsx
+++ components/marketing/features.tsx
@@ -1,1 +1,12 @@
-export const Features = () => "features";
+export const Features = () => {
+    return (
+        <div className="bg-black text-white">
+            <div className="contanier">
+                <h2>evrifing you need</h2>
+                <P>informacje dalsze </P>
+            </div>
+
+        </div>
+
+    );
+};
