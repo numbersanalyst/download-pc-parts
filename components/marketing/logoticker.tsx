d206a3a4defbe413095310d1cb2936af2839e394
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2025-03-18 11:29:38 +0100

    Add temponary logos for logoticker

--- components/marketing/logoticker.tsx
+++ components/marketing/logoticker.tsx
@@ -1,18 +1,13 @@
 import Image from "next/image";
-import acmeLogo from "../assets/images/acme.png";
-import quantumLogo from "../assets/images/quantum.png";
-import echoLogo from "../assets/images/echo.png";
-import celestialLogo from "../assets/images/celestial.png";
-import pulseLogo from "../assets/images/pulse.png";
-import apexLogo from "../assets/images/apex.png";
+
+import amdLogo from "@/public/logos/amd-logo.svg";
+import intelLogo from "@/public/logos/intel-logo.svg";
+import nvidiaLogo from "@/public/logos/nvidia-logo.svg";
 
 const images = [
-    { src: acmeLogo, alt: "Acme Logo" },
-    { src: quantumLogo, alt: "Quantum Logo" },
-    { src: echoLogo, alt: "Echo Logo" },
-    { src: celestialLogo, alt: "Celestial Logo" },
-    { src: pulseLogo, alt: "Pulse Logo" },
-    { src: apexLogo, alt: "Apex Logo" }
+    { src: amdLogo, alt: "AMD Logo" },
+    { src: intelLogo, alt: "Intel Logo" },
+    { src: nvidiaLogo, alt: "Nvidia Logo" },
 ];
 
 export const LogoTicker = () => {
@@ -25,12 +20,12 @@ export const LogoTicker = () => {
                 <div className="overflow-hidden mt-9 relative before:content-[''] after:content-[''] before:absolute after:absolute before:h-full before:w-5 after:w-5 before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))]">
                     <div className="flex gap-16">
                         {images.map((image, index) => (
-                            <Image 
-                                key={index} 
-                                src={image.src} 
-                                alt={image.alt} 
-                                width={100} 
-                                height={40} 
+                            <Image
+                                key={index}
+                                src={image.src}
+                                alt={image.alt}
+                                width={100}
+                                height={40}
                                 className="flex-none w-auto"
                             />
                         ))}
