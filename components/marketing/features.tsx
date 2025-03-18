b6bb1934da33b3b649156d9f7eb4adfa16718c50
olimpiaoli127311 <124042636+olimpiaoli127311@users.noreply.github.com>
2025-03-18 01:36:13 +0100

    update

--- components/marketing/features.tsx
+++ components/marketing/features.tsx
@@ -1,12 +1,33 @@
+const features = [
+    {
+        title: 'Feature 1',
+        description: 'Description of feature 1'
+    },
+    {
+        title: 'Feature 2',
+        description: 'Description of feature 2'
+    },
+    {
+        title: 'Feature 3',
+        description: 'Description of feature 3'
+    }
+];
+
 export const Features = () => {
     return (
         <div className="bg-black text-white">
-            <div className="contanier">
-                <h2>evrifing you need</h2>
-                <P>informacje dalsze </P>
+            <div className="container">
+                <h2>Everything you need</h2>
+                <p>Informacje dalsze</p>
+                <div className="features-list">
+                    {features.map((feature, index) => (
+                        <div key={index} className="feature-item">
+                            <h3>{feature.title}</h3>
+                            <p>{feature.description}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
-
         </div>
-
     );
 };
