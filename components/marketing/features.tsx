3a6423a16a6f8b503288f50b26eed184fe0e73df
Numbers analyst <mrgregorybrzeczyszczykiewicz@proton.me>
2025-03-24 21:57:35 +0100

    Fix design of features section

--- components/marketing/features.tsx
+++ components/marketing/features.tsx
@@ -20,24 +20,26 @@ const features = [
 
 export const Features = () => {
   return (
-    <div className="bg-black text-white py-[72px]">
+    <div className="bg-black text-white py-[72px] sm:py-24 flex justify-center">
       <div className="container">
-        <h2 className="text-center font-bold text-5xl tracking-tighter">
+        <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">
           Wszystko, czego potrzebujesz
         </h2>
-        <p className="text-center mt-5 text-xl text-white/70">
-          Informacje dalsze
-        </p>
-        <div className="mt-16 flex sm:flex-col flex-row-auto gap-4">
+        <div className="max-w-xl mx-auto">
+          <p className="text-center mt-5 text-xl text-white/70">
+            Informacje dalsze
+          </p>
+        </div>
+        <div className="mt-16 flex flex-col sm:flex-row justify-center gap-4">
           {features.map(({ title, description }) => (
             <div
               key={title}
-              className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
+              className="border border-white/30 px-5 py-14 text-center rounded-xl sm:flex-1 sm:max-w-md"
             >
-              <div className="inline-flex h-14 w-14 bg-white text-black justify-center rounded-lg">
-                <Leaf className="h-full w-full object-cover" />
+              <div className="inline-flex w-14 h-14 bg-white text-black justify-center rounded-lg">
+                <Leaf className="size-full p-3 object-cover" />
               </div>
-              <h3 className="mt-6  font-bold">{title}</h3>
+              <h3 className="mt-6 font-bold">{title}</h3>
               <p className="mt-2 text-white/80">{description}</p>
             </div>
           ))}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
 	],
 	theme: {
 		extend: {
+			container: {
+				padding: '1rem',
+			},
 			colors: {
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
