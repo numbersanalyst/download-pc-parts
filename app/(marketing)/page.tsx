551fe05761fc5111d7338bf60ebecf27a2bdb8d9
Numbers analyst <mrgregorybrzeczyszczykiewicz@proton.me>
2025-03-26 21:52:08 +0100

    Create testimonials section for landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "@/components/marketing/navbar";
 import { Hero } from "@/components/marketing/hero";
 import { LogoTicker } from "@/components/marketing/logo-ticker";
 import { Features } from "@/components/marketing/features";
+import { Testimonials } from "@/components/marketing/testamonials";
 import { ProductShowcase } from "@/components/marketing/product-showcase";
 import { FAQs } from "@/components/marketing/faqs";
 import { CallToAction } from "@/components/marketing/call-to-action";
@@ -16,6 +17,7 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <Features />
+      <Testimonials />
       <ProductShowcase />
       <FAQs />
       <CallToAction />
