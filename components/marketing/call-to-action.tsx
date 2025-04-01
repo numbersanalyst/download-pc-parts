46b716eee839165c50174ef468546d1cab6fcd03
zlixsy <github@op.pl>
2025-04-01 15:11:21 +0200

    update contact section

--- components/marketing/call-to-action.tsx
+++ components/marketing/call-to-action.tsx
@@ -8,7 +8,7 @@ export const CallToAction = () => {
       <div className="container max-w-xl relative">
         <Image src={helixImage} alt="" className="absolute top-6 left-[calc(100%-24px)]" />
         <Image src={emojiStarImage} alt="" className="absolute -top-[120px] right-[calc(100%-24px)]" />
-        <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">Get instant access</h2>
+        <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">Contact with us</h2>
         <p className="text-xl text-white/70 mt-5"></p>
         <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row">
           <input
@@ -16,7 +16,7 @@ export const CallToAction = () => {
             placeholder="your@email.com"
             className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"
           />
-          <button className="bg-white text-black h-12 rounded-lg px-5 shrink-0">Get access</button>
+          <button className="bg-white text-black h-12 rounded-lg px-5 shrink-0">Contact</button>
         </form>
       </div>
     </div>
