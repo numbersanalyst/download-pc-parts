52b15a65201e87dc0b75f7af19a779468f74b8b3
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2025-04-07 10:35:33 +0200

    Change how animation works in carousel

--- components/configuration/select-model-carousel.tsx
+++ components/configuration/select-model-carousel.tsx
@@ -68,7 +68,7 @@ export const SelectModelCarousel = <T extends ItemType>({
                     <CardContent className="flex flex-col items-center justify-center p-6 select-none">
                       <motion.div
                         initial={{ opacity: 0.0, y: 20 }}
-                        whileInView={{ opacity: 1, y: 0 }}
+                        animate={{ opacity: 1, y: 0 }}
                         transition={{
                           delay: 0.2,
                           duration: 0.5,
