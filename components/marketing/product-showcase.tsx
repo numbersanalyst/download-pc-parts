edcbd5070f723eab378a8e25a22db267d97350c1
Numbers analyst <mrgregorybrzeczyszczykiewicz@proton.me>
2025-04-22 22:46:45 +0200

    Add animation on scroll to product showcase section

--- components/marketing/product-showcase.tsx
+++ components/marketing/product-showcase.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import appScreen from "@/public/images/computer.webp";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const appImage = useRef<HTMLImageElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
   return (
     <div
       id="app"
@@ -13,14 +27,24 @@ export const ProductShowcase = () => {
         </h2>
         <div className="max-w-xl mx-auto">
           <p className="text-xl text-center text-white/70 mt-5">
-            We have something you can't find anywhere else. We're changing the way the world works.
+            We have something you can't find anywhere else. We're changing the
+            way the world works.
           </p>
         </div>
-        <Image
-          src={appScreen}
-          alt="The product screenshot"
-          className="mt-14 w-full rounded-xl"
-        />
+        <motion.div
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            src={appScreen}
+            alt="The product screenshot"
+            className="mt-14 w-full rounded-xl"
+            ref={appImage}
+          />
+        </motion.div>
       </div>
     </div>
   );
